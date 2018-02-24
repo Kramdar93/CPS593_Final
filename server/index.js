851ffcc9ba95893b8a932e47e74cc27324f33252
Mark Elsinger <thesmokingamp@gmail.com>
2018-02-24 15:15:33 -0500

    Fixed Static Content Loading Made static path and file paths consistent. Added some comments for readability.

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
+//load express, retrieve app object
 const express = require("express");
 var app = express();
 
+//define server name and port
 const serverName = "localhost";
 const port = 8080;
 
+//load routing controller from simpleController.js
 const simple = require("./simpleController");
 
+//use routing controller to listen on port
 app.use(simple).listen(port);
 
+//remind the developer of our url
 console.log("running on http://" + serverName + ":" + port);
--- server/simpleController.js
+++ server/simpleController.js
@@ -1,40 +1,54 @@
+//load express and save app object
 const express = require("express");
 var app = express.Router();
 
+//export the app object
 module.exports = app
+    //expose node_modules for static access. Todo: only expose what's necessary
+    .use(express.static("node_modules"))
+    //unused middleware loader
     .use(function(req, res, next){
-        //middleware loader
         next();
     })
+    //load in som instances of testContent
     .get("/testContent", function (req, res) {
-        htmlstring = htmlHead();
-        testOBJ = require("./testContent.json");
+        htmlstring = htmlHead;
+        testOBJ = require("./testContent.json"); //load testContent JSON object
         htmlstring += populateContent(testOBJ);
-        htmlstring += htmlTail();
+        htmlstring += populateContent(testOBJ);
+        htmlstring += populateContent(testOBJ);
+        htmlstring += populateContent(testOBJ);
+        htmlstring += populateContent(testOBJ);
+        htmlstring += htmlTail;
         res.write(htmlstring);
         res.end();
-    }); //note: no ; for some reason...
+    })
 
+//Construct a card that is populated with a JSONObj of testContent structure, return html string
 function populateContent(JSONObj){
-    result = "<div class='card text-light bg-primary' style='width: 18rem;'>"
+    //initialize card string with headers.
+    result = "<div class='card text-light bg-primary'>"
               +"<div class='card-body'>"
                 +"<h5 class='card-title'>" + JSONObj.content.title + "</h5>"
-                +"<h6 class='card-subtitle text-muted'>" + JSONObj.user + "</h6>"
+                +"<h6 class='card-subtitle'>" + JSONObj.user + "</h6>"
                 +"<p class='card-text'>" + JSONObj.content.text + "</p>"
                 ;
 
+    //append each image
     for (i = 0; i < JSONObj.content.images.length; ++i)
     {
         result += "<img src='" + JSONObj.images[i].src + "' alt='" + JSONObj.images[i].alt + "' />" ;
     }
 
+    //close card div
     result += "</div></div>" ;
 
+    //return the html string
     return result;
 }
 
-function htmlHead(){
-    return "<!doctype html>"
+//html document header
+htmlHead = "<!doctype html>"
                 +"<html lang='en'>"
                 +"<head>"
                     +"<!-- Required meta tags -->"
@@ -42,19 +56,19 @@ function htmlHead(){
                     +"<meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>"
                 
                     +"<!-- Bootstrap CSS -->"
-                    +"<link rel='stylesheet' href='/node_modules/bootstrap/css/bootstrap.css' crossorigin='anonymous'>"
+                    +"<link rel='stylesheet' type='text/css' href='/bootstrap/dist/css/bootstrap.css'>"
                 
                     +"<title>Can I Get a Fitness</title>"
                     +"<meta name='author' content='Mark Elsinger' />"
                     +"<meta name='description' content='A fitness tracking/social media web app' />"
                 +"</head>"
-                +"<body>";
-}
+                +"<body>"
+                +"<div class='card-columns'>";
 
-function htmlTail(){
-    return "<script src='/node_modules/jquery/dist/jquery.min.js'></script>"
-            +"<script src='/node_modules/popper.js/dist/popper.js'></script>"
-            +"<script src='/node_modules/bootstrap/dist/js/bootstrap.js'></script>"
+//html document end                
+htmlTail = "</div>"
+            +"<script src='/jquery/dist/jquery.min.js'></script>"
+            +"<script src='/popper.js/dist/popper.js'></script>"
+            +"<script src='/bootstrap/dist/js/bootstrap.js'></script>"
         +"</body>"
         +"</html>" ;
-}
