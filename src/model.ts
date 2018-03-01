ef1c94b7ca09e58ebe425231bdef84e0141217c4
Mark Elsinger <thesmokingamp@gmail.com>
2018-03-01 17:47:08 -0500

    Expanded View Made view.ts populate more elements of content. Removed transpiled js files mistakenly included in last commit.

--- src/model.ts
+++ src/model.ts
@@ -7,7 +7,7 @@ export function GetJsonData(){
         "user":"admin",
         "rating":0,
         "content":{
-            "title":"Test Content",
+            "title":"Test Content 1",
             "text":"A social fitness tracker web app that allows the user to track their exercises and progress, share updates and milestones, and comment/rate other users posts. User will be able to create and log in to their account, and maintain a friends list of other users for easier lookup and a more relevant feed.",
             "images":[]
         },
@@ -17,7 +17,7 @@ export function GetJsonData(){
         "user":"admin",
         "rating":0,
         "content":{
-            "title":"Test Content",
+            "title":"Test Content 2",
             "text":"A social fitness tracker web app that allows the user to track their exercises and progress, share updates and milestones, and comment/rate other users posts. User will be able to create and log in to their account, and maintain a friends list of other users for easier lookup and a more relevant feed.",
             "images":[]
         },
@@ -27,7 +27,7 @@ export function GetJsonData(){
         "user":"admin",
         "rating":0,
         "content":{
-            "title":"Test Content",
+            "title":"Test Content 3",
             "text":"A social fitness tracker web app that allows the user to track their exercises and progress, share updates and milestones, and comment/rate other users posts. User will be able to create and log in to their account, and maintain a friends list of other users for easier lookup and a more relevant feed.",
             "images":[]
         },
--- src/view.ts
+++ src/view.ts
@@ -1,20 +1,34 @@
+//load model's method for returning some data
 import { GetJsonData } from "./model";
+
+//load in jquery too
 import * as $ from "jquery";
 
+//once the document is ready 
 $(document).ready(function(){
+    //retrieve the data
     var data = GetJsonData();
-    var cardTemplate;
+    //only display if we got anything.
     if (data.length > 0)
     {
-        $("#postContainer").load("../templates/postCard.html", function(){
-            $(this).find(".card-title").text(data[0].content.title);
-            var next = $(this);
-            for(var i = 1; i < data.length; ++i)
-            {
-                next = next.clone();
-                $(this).parent().append(next);
-                next.find(".card-title").text(data[i].content.title);
-            }
+        //save reference to our container for posts
+        var container = $("#postContainer");
+        //load in template data
+        $.get("../templates/postCard.html", function(template){
+            //once it's loaded, duplicate and populate for each bit of data
+            data.forEach(element => {
+                //append the template, store what was appended by picking the last child.
+                var current = container.append(template).children(":last");
+
+                current.find(".card-title").text(element.content.title);
+                current.find(".card-subtitle").text(element.user);
+                current.find(".card-text").text(element.content.text);
+
+                //append images
+                element.content.images.forEach(image => {
+                    current.append(`<a src='${image.src}' alt='${image.alt}' />`);
+                });
+            });
         });
     }
 });
