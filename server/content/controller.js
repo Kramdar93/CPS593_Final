55186ceb1094f5788a68d4f464030e61e9b71a96
Mark Elsinger <thesmokingamp@gmail.com>
2018-04-16 12:22:02 -0400

    Post Submission Added api post call to push posts onto a users post array. Added bodyparser dependency and told express to use its json and urlencoded Modified launch.json since f5 hotkey stopped working for whatever reason

--- server/content/controller.js
+++ server/content/controller.js
@@ -8,8 +8,15 @@ var Users = require("./Users");
 module.exports = app
     //for /user, get their profile.
     .get("/user/*", (req, res) => {
-        res.send(Users.find(x=>x.user.username == req.path.slice(6)).user )
+        res.send(Users.find(x=>x.user.username == req.path.slice(6) ).user )
     })
     .get("/feed/*", (req,res) => {
-        res.send(Users.find(x=>x.user.username == req.path.slice(6)).posts )
+        res.send(Users.find(x=>x.user.username == req.path.slice(6) ).posts )
+    })
+    .post("/submit", (req,res)=>{
+        var foundUser = Users.find(x=>x.user.username == req.body.username );
+        if(foundUser){
+            foundUser.posts.push(req.body.post); //just push the whole json object given so client handles construction.
+        }
+        res.send({reqUser:req.body.username,fndUser:foundUser,allUsers:Users});//echo users for testing.
     })
--- server/index.js
+++ server/index.js
@@ -1,28 +1,33 @@
-//path for filepath operations
+//libraries
 const path = require("path");
-//load express, retrieve app object
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
 
-//define server name and port
-const serverName = "localhost";
-const port = 8080;
+//library instantiations
+const app = express();
 
 //load routing controller from simpleController.js
 //const simple = require("./simpleController");
 //const simple = require("./mvcRouter");
 const controller = require("./content/controller");
 
+//define server name and port
+const serverName = "localhost";
+const port = 8080;
+
+
 //use routing controller to listen on port
 app
+    //third-party middleware
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded( {extended:false} ))
     .use("/",(req,res,next)=>{
-        //middleware to allow passing more data
+        //custom middleware to allow passing more data
         res.header("Access-Control-Allow-Origin","*");
         res.header("Access-Control-Allow-Methods","*");
         res.header("Access-Control-Allow-Headers","*");
         next();
     })
-    
     .use("/",express.static(path.join(__dirname,"../dist/")))
     .use('/content', controller)
     .listen(port);
