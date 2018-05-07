266e8246b7315889a648ea0812a836d2b33fd5bc
Mark Elsinger <thesmokingamp@gmail.com>
2018-05-07 11:35:05 -0400

    Made Forms Prettier Made margins consistent in new post/friend forms. Could still be lined up with heading but not sure how. Added exercises to profile page.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -9,9 +9,24 @@ import { ContentService } from '../services/content.service';
 })
 export class ProfileComponent implements OnInit {
 
+  totals:{type:string,amount:number}[] = [];
+
   constructor(private contentServer:ContentService) {
     //don't have to even track user now, just use the content service.
-   }
+    contentServer.GetPosts(contentServer.currentUser.userID).subscribe(data=>{
+      console.log(data);
+      for (const post of data.json()) {
+        var index = this.totals.findIndex(x=>x.type==post.content.workout);
+        if (index < 0){ //not found
+          this.totals.push({type:post.content.workout,amount:parseInt(post.content.reps)});
+        }
+        else{ //found so increment.
+          this.totals[index].amount += parseInt(post.content.reps);
+        }
+      }
+    });
+    
+  }
 
   ngOnInit() {
   }
